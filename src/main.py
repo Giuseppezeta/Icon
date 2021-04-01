339984f059553376334cd7c510b5a542d2c2d880
Giuseppe <46595242+Giuseppezeta@users.noreply.github.com>
2021-04-01 19:29:37 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -81,10 +81,6 @@ def Classification_Train(X_train,y_train,model = "KNN"):
   if model == "KNN":
     knn = KNeighborsClassifier(n_neighbors=1, metric='euclidean')
     knn.fit(X_train, y_train)
-    """y_pred_knn_train = knn.predict(X_train)
-    acc_knn_train = accuracy_score(y_train, y_pred_knn_train)
-    print(f'KNN accuracy train = {acc_knn_train}')
-    #X_train, X_test, y_train, y_test = train_test_split(np.array(X),np.array(y), test_size=0.20, random_state=42)"""
     return knn
   elif model == "SVM":
         clf = svm.SVC()
@@ -123,7 +119,6 @@ def get_embedding(model, face_pixels):
 	return yhat[0]
 
 def extract_face_from_image(image):
-    #detector = MTCNN(steps_threshold=[0.8,0.9,0.9])
     detector = MTCNN(min_face_size=80, steps_threshold=[0.98,0.98,0.98])
     faces = detector.detect_faces(image)
 
@@ -245,70 +240,6 @@ def visualizzation(embedded, targets):
   plt.legend(bbox_to_anchor=(1, 1))
   plt.show()
 
-def findEuclideanDistance(source_representation, test_representation):
-    euclidean_distance = source_representation - test_representation
-    euclidean_distance = np.sum(np.multiply(euclidean_distance, euclidean_distance))
-    euclidean_distance = np.sqrt(euclidean_distance)
-    return euclidean_distance
-
-def findCosineDistance(source_representation, test_representation):
-  a = np.matmul(np.transpose(source_representation), test_representation)
-  b = np.sum(np.multiply(source_representation, source_representation))
-  c = np.sum(np.multiply(test_representation, test_representation))
-  return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
-
-def valutation(X_db, y_db, X_new, y_real, dist="c"):
-    FAR = []
-    DIR = []
-    f1 = []
-    acc = []
-    if dist == "e":
-        thresholds = np.arange(0, 130)
-    elif dist == "c":
-        thresholds = np.arange(0.0, 1.0, 0.01)
-    thresholds = np.array(thresholds)
-    for t in thresholds:
-        y_pred = []
-        tot = len(X_new)
-        fp = 0
-        tp = 0
-        for x1 in range(len(X_new)):
-            min_dist = 10000
-            if y_real[x1] == -1:
-                tot = tot - 1
-            for x2 in range(len(X_db)):
-                if dist == "e":
-                    distance = findEuclideanDistance(X_new[x1], X_db[x2])
-                elif dist == "c":
-                    distance = findCosineDistance(X_new[x1], X_db[x2])
-                if (distance < min_dist):
-                    pos = x2
-                    min_dist = distance
-            if (min_dist < t):
-                y_pred.append(y_db[pos])
-                if y_db[pos] == y_real[x1]:
-                    tp = tp + 1
-                else:
-                    fp = fp + 1
-            else:
-                y_pred.append(-1)
-        f1.append(f1_score(y_real,y_pred,average='macro'))
-        acc.append(accuracy_score(y_real,y_pred))
-
-    opt_idx = np.argmax(f1)
-    # Threshold at maximal F1 score
-    opt_tau = thresholds[opt_idx]
-    # Accuracy at maximal F1 score
-    opt_acc = acc[opt_idx]
-
-    plt.plot(thresholds, f1, label='f1 score');
-    plt.plot(thresholds, acc, label='accuracy score');
-    plt.axvline(x=opt_tau, linestyle='--', lw=1, c='lightgrey', label='Threshold')
-    plt.title(f'Accuracy at threshold {opt_tau:.2f} = {opt_acc:.3f}');
-    plt.xlabel('Distance threshold')
-    plt.legend();
-    plt.show()
-
 vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
 
 output_folder = 'outputfolder'
@@ -317,29 +248,25 @@ train = r'\Users\Giuseppe\Desktop\IC\train'
 test = r'\Users\Giuseppe\Desktop\IC\test'
 out2 = r'\Users\Giuseppe\Desktop\outputfolder2'
 
-test2 = r'\Users\Giuseppe\Desktop\Testset\t1'
-out3 = r'\Users\Giuseppe\Desktop\outputfolder3'
-
 y_true = [1,2,1,3,3,1,1,4,4,5,6,7,8,8,9,10,11,11,12,12,3,13,3,14,15,16,16,17,18,18,19,19,20,20,7,21,2,7,22,22,11,11,15,21,12,12,23,20,8,24,4,4,22,14,6,24,7,18,13,16,19,19,17,17,5,5,7,15,21,4,16,5,16,16,16,17,19,19,19,13,13,20,19,24,10,18,8,18,22,14,11,17,6,20,14,13,13,21,21,21,8,8,10,22,15,12]
 y_true_test = [1,14,11,11,6,4,4,5,5,19,6,19,3,3,1,1,6,7,7,22,22,15,14,20,20,16,16,17,17,3,18,18,13,13,3,8,8,21,9,10,24,12,12,11,11,15,1,16,12,16,12,5,14,18,4,24,18,22,22,4,20,13,13,21,8,8,5,18,19,19,17,19,15,19,11,12,12,18,17,5,5,17,7,7,7,24,20,17,16,19,19,10,8,10,23,13,8,8,4,22,22,21,2,24,8,7,7,15,13,13,21,21,12,20]
 y_true = asarray(y_true)
 y_true_test = asarray(y_true_test)
 
-"""
 start = time.time()
 descriptors, img_paths = face_ex(train)
 print("--- %s seconds ---" % (time.time() - start))
 pickle.dump( descriptors, open( "descriptors.p", "wb" ) )
 pickle.dump( img_paths, open( "img_paths.p", "wb" ) )
-"""
+
 descriptors = pickle.load( open( "descriptors.p", "rb" ) )
 img_paths = pickle.load( open( "img_paths.p", "rb" ) )
-"""
+
 start = time.time()
 desc, labels, paths = new_clust(descriptors, y_true)
 desc1, labels1, paths1 = new_clust(descriptors, y_true,"k-means")
-desc2, labels2, paths2 = new_clust(descriptors, y_true, "h")"""
-"""
+desc2, labels2, paths2 = new_clust(descriptors, y_true, "h")
+
 print("--- %s seconds ---" % (time.time() - start))
 pickle.dump( desc, open( "desc.p", "wb" ) )
 pickle.dump( desc1, open( "desc1.p", "wb" ) )
@@ -353,7 +280,7 @@ pickle.dump( paths1, open( "paths1.p", "wb" ) )
 descriptors1, img_paths1 = face_ex(test,out2)
 pickle.dump( descriptors1, open( "descriptors1.p", "wb" ) )
 pickle.dump( img_paths1, open( "img_paths1.p", "wb" ) )
-"""
+
 desc = pickle.load( open( "desc.p", "rb" ) )
 labels = pickle.load( open( "labels.p", "rb" ) )
 desc1 = pickle.load( open( "desc1.p", "rb" ) )
